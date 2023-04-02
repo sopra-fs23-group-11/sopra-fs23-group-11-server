6133ba992b53c76214aec16066392f2d42b216bc
qdai <qing.dai>
2023-04-02 14:40:30 +0200

    update grid, lobby, position, ship add new helper class

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Lobby.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/Lobby.java
@@ -2,6 +2,8 @@ package ch.uzh.ifi.hase.soprafs23.entity;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Random;
+
 @Entity
 @Table(name = "Lobby")
 public class Lobby {
@@ -21,6 +23,12 @@ public class Lobby {
 
     public long getLobbyId() {
         return lobbyId;
+    }
+
+    public Lobby() {
+        // generate random 4-digit lobby code
+        Random random = new Random();
+        this.lobbyCode = String.format("%04d", random.nextInt(10000));
     }
 
     public String getLobbyCode() {

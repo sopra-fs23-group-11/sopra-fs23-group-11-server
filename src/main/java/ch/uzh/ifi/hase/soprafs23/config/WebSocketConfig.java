3940f9731b5bb5d6978e24cfa1e38d5d210829ff
nazek <nazekalolabi@gmail.com>
2023-04-18 17:52:53 +0200

    readd the /shot-simple Kalil defined

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/chatroom","/user", "/shot");
+        registry.enableSimpleBroker("/chatroom","/user", "/shot", "/errors", "/shot-simple");
         registry.setUserDestinationPrefix("/user");
     }
 

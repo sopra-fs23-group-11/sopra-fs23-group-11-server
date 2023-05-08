0915c1bf8c75ba581f341b094c5e65d3c0f53d20
nazek <nazekalolabi@gmail.com>
2023-05-08 21:46:25 +0200

    just a check

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/User.java
@@ -4,7 +4,6 @@ import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.time.LocalDate;
 
 /**
  * Internal User Representation
